bb5dd0c18f4eed9b0707d8154be5bfe5ce4732b1
Vlad Chirila <vlad.chirila@live.com>
2023-04-15 08:21:08 +0300

    Update source code

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const clientId = "CHANGEME";
 const clientSecret = "CHANGEME";
 const redirectUri = "http://localhost:1234/redirect.html";
-const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=read_all`;
-const activityList = document.getElementById("activity-list");
 
-function authenticate() {
-  // Redirect user to the Strava authentication page.
-  window.location.href = authUrl;
-}
+function init() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const activityId = urlParams.get("activity_id");
+  
+    if (activityId) {
+      const accessToken = localStorage.getItem("strava_access_token");
+      if (accessToken) {
+        fetchActivity(activityId, accessToken);
+      } else {
+        console.error("No access token found in local storage");
+      }
+    } else {
+      handleAuthCode();
+    }
+  }
+  
+window.onload = init;
 
-async function getAccessToken(code) {
+async function getAccessToken(authCode) {
   const response = await fetch("https://www.strava.com/oauth/token", {
     method: "POST",
     headers: {
@@ -18,10 +29,15 @@ async function getAccessToken(code) {
     body: JSON.stringify({
       client_id: clientId,
       client_secret: clientSecret,
-      code: code,
+      code: authCode,
       grant_type: "authorization_code",
     }),
   });
+
+  if (!response.ok) {
+    console.error(`Error getting access token: ${response.statusText}`);
+    return null;
+  }
 
   const data = await response.json();
   return data.access_token;
@@ -34,91 +50,84 @@ async function fetchActivities(accessToken) {
     },
   });
 
+  if (!response.ok) {
+    console.error(`Error fetching activities: ${response.statusText}`);
+    return;
+  }
+
   const activities = await response.json();
-  displayActivities(activities);
+
+  if (Array.isArray(activities)) {
+    displayActivities(activities);
+  } else {
+    console.error(`Unexpected response from Strava API:`, activities);
+  }
+}
+
+async function fetchActivity(activityId, accessToken) {
+  const response = await fetch(`https://www.strava.com/api/v3/activities/${activityId}`, {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    console.error(`Error fetching activity: ${response.statusText}`);
+    return;
+  }
+
+  const activity = await response.json();
+  displayActivityDetails(activity);
+}
+
+function displayActivityDetails(activity) {
+  const distanceInKm = (activity.distance / 1000).toFixed(3); // Convert meters to kilometers
+  document.getElementById("activity-name").textContent = activity.name;
+  document.getElementById("activity-date").textContent = new Date(activity.start_date).toUTCString();
+  document.getElementById("activity-distance").textContent = distanceInKm;
+  document.getElementById("activity-elevation").textContent = activity.total_elevation_gain;
+  document.getElementById("activity-time").textContent = `${Math.floor(activity.moving_time / 60)} minutes`;
 }
 
 function displayActivities(activities) {
+  const activityList = document.getElementById("activity-list");
+
   activities.forEach((activity) => {
+    const distanceInKm = (activity.distance / 1000).toFixed(3); // Convert meters to kilometers
     const listItem = document.createElement("li");
-    listItem.textContent = `${activity.name} - ${activity.distance} meters - ${activity.total_elevation_gain} meters elevation gain`;
+    listItem.textContent = `${activity.name} - ${distanceInKm} km - ${activity.total_elevation_gain} meters elevation gain`;
+    listItem.addEventListener("click", () => {
+      window.location.href = `activity-detail.html?activity_id=${activity.id}`;
+    });
     activityList.appendChild(listItem);
   });
-  }
-  
-  function handleAuthCode() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const authCode = urlParams.get("code");
-    const activityId = urlParams.get("activity_id");
-    
-    if (authCode) {
+}
+
+function authenticate() {
+  const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=activity:read_all`;
+  window.location.href = authUrl;
+}
+
+function handleAuthCode() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const authCode = urlParams.get("code");
+
+  if (authCode) {
     getAccessToken(authCode).then((accessToken) => {
-    // Store the access token in localStorage for easy access across pages
-    localStorage.setItem("strava_access_token", accessToken);
-    if (activityId) {
-    fetchActivityDetail(accessToken, activityId);
-    } else {
-    fetchActivities(accessToken);
-    }
+      if (accessToken) {
+        localStorage.setItem("strava_access_token", accessToken);
+        // Redirect back to index.html with the access token
+        window.location.href = `index.html?access_token=${accessToken}`;
+      } else {
+        console.error("Error obtaining access token");
+      }
     });
-    } else if (activityId) {
-    const accessToken = localStorage.getItem("strava_access_token");
+  } else {
+    const accessToken = urlParams.get("access_token");
     if (accessToken) {
-    fetchActivityDetail(accessToken, activityId);
+      fetchActivities(accessToken);
     } else {
-    authenticate();
-    }
-    } else {
-    authenticate();
+      authenticate();
     }
   }
- 
-async function fetchActivityDetail(accessToken, activityId) {
-    const response = await fetch(`https://www.strava.com/api/v3/activities/${activityId}`, {
-      headers: {
-        "Authorization": `Bearer ${accessToken}`,
-      },
-    });
-  
-    const activity = await response.json();
-    displayActivityDetail(activity);
-  }
-  
-  function displayActivityDetail(activity) {
-    const activityName = document.getElementById("activity-name");
-    const activityMap = document.getElementById("activity-map");
-    const activityStats = document.getElementById("activity-stats");
-  
-    activityName.textContent = activity.name;
-  
-    // Display activity stats
-    const stats = [
-      { label: "Distance", value: `${activity.distance} meters` },
-      { label: "Elevation Gain", value: `${activity.total_elevation_gain} meters` },
-      { label: "Moving Time", value: `${activity.moving_time} seconds` },
-      { label: "Elapsed Time", value: `${activity.elapsed_time} seconds` },
-    ];
-  
-    stats.forEach((stat) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = `${stat.label}: ${stat.value}`;
-      activityStats.appendChild(listItem);
-    });
-  }
-  
-  // Modify the displayActivities function in script.js to open the activity detail page
-  
-  function displayActivities(activities) {
-    activities.forEach((activity) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = `${activity.name} - ${activity.distance} meters - ${activity.total_elevation_gain} meters elevation gain`;
-      listItem.addEventListener("click", () => {
-        window.location.href = `activity-detail.html?activity_id=${activity.id}`;
-      });
-      activityList.appendChild(listItem);
-    });
-  }
-  
-   
-  // Initialize the app
-  handleAuthCode();
+}
